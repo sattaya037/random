67973125b24502e044e4ba12024a54f5a05dadb4
sattaya rakjan <sattaya.ra@ics-th.com>
2019-09-17 14:47:55 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,34 +14,37 @@ class App extends Component {
 
 class Counter extends Component {
   state = {
-    count: 0, //กำหนด จำนวนนับเวลากดปุ่ม ให้เป็น 0
-    array:Array.from(Array(100).keys()) //กำหนด ค่า ใน Array อันนี้ให้มันมีเลขจำนวน 0-99 เก็บไว้ใน state ที่ชื่อว่า array 
+    count: 0,
+    array:Array.from(Array(100).keys())
   };
-  handleClick = () => { //ฟังก์ชั่น เวลากดปุ่ม
+  handleClick = () => { 
     this.setState((prevState, { count }) => ({
-      count: prevState.count + 1 //พอกดปุ่ก็ให้ count + 1 กด 2 ครั้งก็บ +2...+3 ไปเรื่อยๆ
+      count: prevState.count + 1
     }));
-    this.array()//เรียกใช้ฟังก์ชั้น array
+    this.array()
   };
   array() {
-    console.log(this.state.array)
-    var a = this.state.array //ประกาศตัวแปร a ให้เท่ากับ state ของ array
-    if(this.state.count===1){ //ถ้า state ของ count == 1 (หรือกดปุ่ม 1 ครั้ง) ให้เข้าเงื่อนไขนี้
-        a.splice(Math.floor(Math.random()*a.length), 50);  //เอา ชุดข้อมูลของ state array มาตัด อันนี้มึงไปหาดูเอาเองละกันคำสั่งนี้ กูก็ไปก็อปเขามา
-        this.setState({ array: a }) 
-        console.log("click 1: "+a);
-    }else if(this.state.count===2){ //เงื่อนไขเมื่อ กดปุ่ม 2 ครั้ง
-        a.splice(Math.floor(Math.random()*a.length), 25);
-        this.setState({ array: a })  //กำหนด state array เข้าไปใหม่
-        console.log("click 2: "+a);
-    }else if(this.state.count===3){ //เงื่อนไขเมื่อ กดปุ่ม 3 ครั้ง
-        a.splice(Math.floor(Math.random()*a.length), 10);
-        this.setState({ array: a }) 
-        console.log("click 3: "+a);
-    }else if(this.state.count===4){ //เงื่อนไขเมื่อ กดปุ่ม 4 ครั้ง
-      a.splice(Math.floor(Math.random()*a.length), 5);
-      this.setState({ array: a }) 
+    var a = this.state.array
+    var index = Math.floor( Math.random()*a.length );
+   
+    if(this.state.count===1){
+      console.log("array: "+a);
+      console.log("length: "+a.length);
+        a.splice( index, 50 ); // Remove the item from the array
+      console.log("click 1: "+a);
+      console.log("length: "+a.length);
+    }else if(this.state.count===2){
+      a.splice( index, 25 ); // Remove the item from the array
+      console.log("click 2: "+a);
+      console.log("length: "+a.length);
+    }else if(this.state.count===3){
+      a.splice( index, 5 ); // Remove the item from the array
+      console.log("click 3: "+a);
+      console.log("length: "+a.length);
+    }else if(this.state.count===3){
+      a.splice( index, 2 ); // Remove the item from the array
       console.log("click 4: "+a);
+      console.log("length: "+a.length);
     }
   }
  
